03521a808a9be9fea18dda42838ba1138e554c66
DEV JANGIR <11ganeshji11@gmail.com>
2025-07-19 21:28:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "my-unique-bucket-name-12345"  # must be globally unique
+  bucket = "myuniquebucketdemotfname12345"  # must be globally unique
 
   tags = {
     Name        = "TerraformExample"
