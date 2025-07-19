746f158ddf44ca165023655cba24e6dc175c2ddd
DEV JANGIR <11ganeshji11@gmail.com>
2025-07-19 20:52:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # or your preferred region
+}
+
 resource "aws_s3_bucket" "demo" {
   bucket = "my-tf-test-bucket"
 
